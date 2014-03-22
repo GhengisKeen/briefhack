9d8ee6ea14de03746ea22a353884144f1782a4c3
Christopher Little <hephistocles@gmail.com>
2014-03-22 18:34:27 +0000

    Bring in hard-coded news articles on the list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,18 +3,39 @@
 angular.module('briefhackApp')
 	.controller('MainCtrl', function($scope, $http) {
 		$scope.selected = [];
-		$scope.newsList = [{
-				name: 'newsAAA',
-				isActive: false
-			}, {
-				name: 'newsAAA',
-				isActive: false
-			}, {
-				name: 'newsAAA',
-				isActive: false
-			}];
 
-			    $scope.isActive = false;
+		// Authenticate
+		$http.get('https://amers1.mobile13.cp.reutest.com/msf1.0/data/UserInfo');
+
+		// Get news item from hard-coded category
+		$scope.newsList = [];
+		$http.get('https://amers1.mobile13.cp.reutest.com/msf1.0/data/TopNews', {
+			params: {
+				"$filter":"Request/Codes eq 'urn:newsml:reuters.com:20060725:SPDOC_304469252006'",
+				"$select":"headline,ImageUrl,SmallImageUrl,Brief,uniqueIdentifier"
+			}
+		}).
+	    success(function(data, status, headers, config) {
+	    	for (var index = 0; index < data.d.length; index++) {
+			    console.log(data.d[index]);
+			}
+	    	for (var i = 0; i < data.d.length; i++) {
+				$scope.newsList.push(
+	    			{
+	    				name: data.d[i].headline,
+	    				headline: data.d[i].headline,
+	    				excerpt: data.d[i].Brief,
+	    				code: data.d[i].uniqueIdentifier,
+	    				isActive:false
+	    			}
+	    		);
+	    	}
+	    }).
+	    error(function(data, status, headers, config) {
+	    	debugger;
+	    });
+
+		$scope.isActive = false;
 /*		$scope.btnAdd = function() {
 			// $window.alert(1);
 			console.log('hihi');
