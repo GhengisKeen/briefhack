ffcbaa2189c8770ef728cc49176f1855cb5e51b5
Daniel Low <daniellowtw@gmail.com>
2014-03-22 15:52:13 +0000

    first buttons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,38 @@
 'use strict';
 
 angular.module('briefhackApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
+	.controller('MainCtrl', function($scope, $http) {
+		$scope.newsList = [{
+				name: 'newsAAA',
+				clickedClass: 'clickedClass'
+			}, {
+				name: 'newsAAA',
+				clickedClass: 'clickedClass'
+			}, {
+				name: 'newsAAA',
+				clickedClass: 'clickedClass'
+			}];
+/*		$scope.btnAdd = function() {
+			// $window.alert(1);
+			console.log('hihi');
+			$scope.newsList = [{
+				name: 'newsAAA',
+				clickedClass: false
+			}, {
+				name: 'newsAAA',
+				clickedClass: false
+			}, {
+				name: 'newsAAA',
+				clickedClass: false
+			}];
+		}*/
+		$scope.highlightMe = function($index) {
+			// $window.alert(1);
+			console.log($index);
+			$scope.newsList[$index].clickedClass=true;
+		}
+		// $http.get('/api/awesomeThings').success(function(awesomeThings) {
+		// 	$scope.awesomeThings = awesomeThings;
+		// });
+		// $scope.newsList = ['newsA', 'newsB', 'newsC'];
+	});
