ae4c2e63e7b9b85e5c5922f6f74da722c351cc3a
Daniel Low <daniellowtw@gmail.com>
2014-03-22 17:51:08 +0000

    created client view and controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,38 +4,28 @@ angular.module('briefhackApp')
 	.controller('MainCtrl', function($scope, $http) {
 		$scope.selected = [];
 		$scope.newsList = [{
-				name: 'newsAAA',
-				isActive: false
-			}, {
-				name: 'newsAAA',
-				isActive: false
-			}, {
-				name: 'newsAAA',
-				isActive: false
-			}];
+			name: 'newsAAA',
+			headline: 'newsAAA',
+			excerpt: 'newsAAA',
+			code: 'newsAAA',
+			isActive: false
+		}, {
+			name: 'newsAAA',
+			isActive: false
+		}, {
+			name: 'newsAAA',
+			isActive: false
+		}];
 
-			    $scope.isActive = false;
-/*		$scope.btnAdd = function() {
-			// $window.alert(1);
-			console.log('hihi');
-			$scope.newsList = [{
-				name: 'newsAAA',
-				isActive: false
-			}, {
-				name: 'newsAAA',
-				isActive: false
-			}, {
-				name: 'newsAAA',
-				isActive: false
-			}];
-		}*/
+		$scope.isActive = false;
 		$scope.highlightMe = function($index) {
 			// $window.alert(1);
 			console.log($index);
-			$scope.newsList[$index].isActive=!$scope.newsList[$index].isActive;
-		}
-		// $http.get('/api/awesomeThings').success(function(awesomeThings) {
-		// 	$scope.awesomeThings = awesomeThings;
-		// });
+			$scope.selected.push($scope.newsList[$index]);
+			$scope.newsList[$index].isActive = !$scope.newsList[$index].isActive;
+		};
+		$http.get('/api/awesomeThings').success(function(awesomeThings) {
+			$scope.awesomeThings = awesomeThings;
+		});
 		// $scope.newsList = ['newsA', 'newsB', 'newsC'];
 	});
