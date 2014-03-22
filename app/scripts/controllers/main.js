a44a3212067ff58f62e99c4d919f1c46e41eef21
Daniel Low <daniellowtw@gmail.com>
2014-03-22 16:11:53 +0000

    working toggle class

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,34 +2,37 @@
 
 angular.module('briefhackApp')
 	.controller('MainCtrl', function($scope, $http) {
+		$scope.selected = [];
 		$scope.newsList = [{
 				name: 'newsAAA',
-				clickedClass: 'clickedClass'
+				isActive: false
 			}, {
 				name: 'newsAAA',
-				clickedClass: 'clickedClass'
+				isActive: false
 			}, {
 				name: 'newsAAA',
-				clickedClass: 'clickedClass'
+				isActive: false
 			}];
+
+			    $scope.isActive = false;
 /*		$scope.btnAdd = function() {
 			// $window.alert(1);
 			console.log('hihi');
 			$scope.newsList = [{
 				name: 'newsAAA',
-				clickedClass: false
+				isActive: false
 			}, {
 				name: 'newsAAA',
-				clickedClass: false
+				isActive: false
 			}, {
 				name: 'newsAAA',
-				clickedClass: false
+				isActive: false
 			}];
 		}*/
 		$scope.highlightMe = function($index) {
 			// $window.alert(1);
 			console.log($index);
-			$scope.newsList[$index].clickedClass=true;
+			$scope.newsList[$index].isActive=!$scope.newsList[$index].isActive;
 		}
 		// $http.get('/api/awesomeThings').success(function(awesomeThings) {
 		// 	$scope.awesomeThings = awesomeThings;
